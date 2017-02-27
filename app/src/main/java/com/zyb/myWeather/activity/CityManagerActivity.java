82e4c3e30c9e71cb183ec8040f89aaa95a12f4f3
zyb <zybns@qq.com>
2017-02-27 17:45:44 +0800

    。

---
+++ app/src/main/java/com/zyb/myWeather/activity/CityManagerActivity.java
@@ -0,0 +1,11 @@
+package com.zyb.myWeather.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/2/27 0027.
+ */
+
+public class CityManagerActivity extends Activity {
+
+}
